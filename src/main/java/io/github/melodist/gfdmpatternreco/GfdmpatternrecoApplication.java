f42d08029f89623d8ceb4edfc60934568bdf60f7
melodist <melt.ep00@gmail.com>
2022-11-13 23:58:28 +0900

    Initial commit

---
+++ src/main/java/io/github/melodist/gfdmpatternreco/GfdmpatternrecoApplication.java
@@ -0,0 +1,13 @@
+package io.github.melodist.gfdmpatternreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfdmpatternrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfdmpatternrecoApplication.class, args);
+	}
+
+}
